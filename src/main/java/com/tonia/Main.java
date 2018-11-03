525e1320b0dbb8bf8f17bbc77727bfecb0c73495
Task_1_1 <cooltone4ka@gmail.com>
2018-11-03 16:42:14 +0200

    /

--- src/main/java/com/tonia/Main.java
+++ src/main/java/com/tonia/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-                   Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
             int number = scanner.nextInt();
             int sum = 0;
             for(int i = 1; i<= number; i++){
