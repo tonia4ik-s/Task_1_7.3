6898e1a75019b49bdca6f630003f316b2a50f6f6
Task_1_1 <cooltone4ka@gmail.com>
2018-11-04 19:05:59 +0200

    /

--- src/main/java/com/tonia/Main.java
+++ src/main/java/com/tonia/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
             int number = scanner.nextInt();
-            int sum = 0;
+            int faktorial = 1;
             for(int i = 1; i<= number; i++){
-                sum = sum + i;
+                faktorial = faktorial * i;
             }
-            System.out.println(sum);
+            System.out.println(faktorial);
             }
 
 }
